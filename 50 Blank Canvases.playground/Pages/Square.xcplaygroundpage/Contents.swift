6c492de316cd397b29b8c2872e8e27037af38445
Lee Andrew <alee@lcs-mba3087.lcs.on.ca>
2017-11-16 09:29:35 -0500

    Make a Square

--- 50 Blank Canvases.playground/Pages/Square.xcplaygroundpage/Contents.swift
+++ 50 Blank Canvases.playground/Pages/Square.xcplaygroundpage/Contents.swift
@@ -1,5 +1,5 @@
 //: [Previous](@previous) / [Next](@next)
-//: # A Blank Canvas
+//: # Squere
 //:
 //: Use this page to experiment. Have fun!
 /*:
@@ -22,7 +22,33 @@ let canvas = Canvas(width: 400, height: 300)
  
  Use whitespace and comments as appropriate.
  */
-// Replace this comment with your first comment – what is the goal of the code you're about to write?
+// Draw Square
+
+// Step 1 Move to start point
+canvas.translate(byX: 75, byY: 100)
+canvas.drawAxes()
+canvas.defaultLineWidth = 10
+
+// Step 2 Make a side
+canvas.drawLine(fromX: 0, fromY: 0, toX: 200, toY: 0)
+canvas.translate(byX: 200, byY: 0)
+canvas.rotate(by: 90)
+
+// Step 3 Make a side
+canvas.drawLine(fromX: 0, fromY: 0, toX: 200, toY: 0)
+canvas.translate(byX: 200, byY: 0)
+canvas.rotate(by: 90)
+
+// Step 4 Make a side
+canvas.drawLine(fromX: 0, fromY: 0, toX: 200, toY: 0)
+canvas.translate(byX: 200, byY: 0)
+canvas.rotate(by: 90)
+
+// Step 5 make a side
+canvas.drawLine(fromX: 0, fromY: 0, toX: 200, toY: 0)
+canvas.translate(byX: 200, byY: 0)
+canvas.rotate(by: 90)
+
 
 
 
